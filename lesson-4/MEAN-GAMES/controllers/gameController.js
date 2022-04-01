b90f46bd2beb357bc6ac28946a62a140631c578c
Meresa <meresa27@gmail.com>
2022-03-31 21:21:29 -0500

    addone implemented in gameController

--- lesson-4/MEAN-GAMES/controllers/gameController.js
+++ lesson-4/MEAN-GAMES/controllers/gameController.js
@@ -17,7 +17,7 @@ module.exports.getAll = (req, res) => {
     }
     gameCollection.find().skip(offset).limit(count).toArray(function(err, games) {
         if(err) {
-            return res.status(500).json({error : "Couldn't get the datas"})
+            return res.status(500).json({error : err})
 
         }
         console.log("Found games", games);
@@ -27,7 +27,38 @@ module.exports.getAll = (req, res) => {
 }
 
 module.exports.addOne = (req, res) => {
-    return res.status(200).json(req.body);
+    console.log("Add One Controller called");
+    const db= dbConnection.get();
+    console.log("db", db);
+    const gameCollection= db.collection("games");
+    let newGame = {};
+    if(req.body && req.body.title&& req.body.price && req.body.players && req.body.age ) {
+        
+        if(parseInt(req.body.players) >= 1 && parseInt(req.body.players) <= 11 && parseInt(req.body.age) >= 6 && parseInt(req.body.age) <= 99) {
+
+            newGame.title = req.body.title;
+            newGame.price = parseFloat(req.body.price);
+            newGame.playes = parseInt(req.body.players, 10);
+            newGame.age = parseInt(req.body.age, 10);
+
+            gameCollection.insertOne(newGame, function(err, response) {
+                if (err) {
+                res.status(500).json({error: err});
+                } else {
+                console.log(response);
+                res.status(201).json(response);
+                }
+
+            });
+        }
+        else {
+            return res.status(500).json({error : "Age must be between 6 and 99 or players must be between 1 to 11"});
+        }
+    }
+    else {
+        return res.status(404).json({error : "No body Submited"});
+    }
+    
 }
 
 module.exports.getOne = (req, res) => {
