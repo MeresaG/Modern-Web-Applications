54e7052ae409724defc5fe9ee7a5684ae553ea8b
Meresa <meresa27@gmail.com>
2022-04-01 23:16:17 -0500

    modified course controller

--- lesson-5/SchoolApp/api/controllers/courseController.js
+++ lesson-5/SchoolApp/api/controllers/courseController.js
@@ -4,33 +4,25 @@ const Student = mongoose.model(process.env.STUDENT_MODEL);
 
 getAll = (req, res) => {
     console.log("Get All Controller called");
-    let offset = 0;
-    let count = 2;
-    if(req.query && req.query.offset) {
-        offset = parseInt(req.query.offset, 10);
-    }
-    if(req.query && req.query.count) {
-        count = parseInt(req.query.count, 10);
-        count = count > 10 ? 10 : count;
-    }
-    Student.find().skip(offset).limit(count).exec(function(err, students) {
-        if(err) {
-            return res.status(500).json({error : err})
+    console.log("GET One Publisher controller");
 
-        }
-        console.log("Found students");
-        return res.status(200).json(students);
-        });
+    const studentId = req.params.studentId;
+
+    Student.findById(studentId).select('course').exec(function(err, student){
+        console.log("Student Found");
+        res.status(200).json(student.course)
+    })
 }
 
 const getOne = (req, res) => {
     console.log("GET One Publisher controller");
 
     const studentId = req.params.studentId;
+    const courseId = req.params.courseId;
 
-    Student.findById(studentId).select('course').exec(function(err, course){
+    Student.findById(studentId).select('course').exec(function(err, student){
         console.log("Student Found");
-        res.status(200).json(course)
+        res.status(200).json(student.course.id(courseId))
     })
 } 
 
